8a1dbefe875077ec0723169bd90912e1d6ea1650
blaine1726 <blainemuri@gmail.com>
2016-09-06 17:07:39 -0700

    got rid of what was breaking heroku

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :chat
-  validates :user, presence: true
-  validates :text, presence: true
+  # validates :user, presence: true
+  # validates :text, presence: true
 end
