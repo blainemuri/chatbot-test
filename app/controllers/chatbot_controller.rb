827f6e817c73414324db90e117f7763146513241
Douglas Yan <douglasyan@Douglass-MacBook-Pro.local>
2016-09-16 10:04:12 -0700

    Updated Credentials

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -11,13 +11,13 @@ class ChatbotController < ApplicationController
     # into the body data
     @body = query.to_json
 
-    uri = URI.parse("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/bcee86e6-d69c-4eb9-be40-a3ce769450df/message?version=2016-07-11")
+    uri = URI.parse("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/19d05bd9-53a2-427f-9091-a74b885eef26/message?version=2016-07-11")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Post.new(uri.request_uri)
     request.add_field('Content-Type', 'application/json')
-    request.basic_auth("b42cc929-33b8-462c-9cf7-d42cba172c56", "CXMTz6p7nv2w")
+    request.basic_auth("a4da0ece-5ee3-4e11-87ed-e6afe3ed8b8c", "IeUUcCDdkoMH")
     request.body = @body
     response = http.request(request)
 
